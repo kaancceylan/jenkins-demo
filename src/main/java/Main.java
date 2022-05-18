47ba3e65ea3e1ffc388858a143e9fad69ecbf56d
Kaan Ceylan <70446695+kaancceylan@users.noreply.github.com>
2022-05-18 17:37:18 +0300

    Delete Main.java

--- src/main/java/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-        public static void main(String[] args) {
-            //code will go in here
-        }
-    }
-
-public static boolean checkIfInputIsAnEvenNumber(int number){
-        return number % 2 == 0;
-    }
